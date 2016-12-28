4e16a576cd910cc1059075e5f1c7d95e0894dbf0
Maciek Rylko <rylko.maciej@gmail.com>
2016-12-28 21:37:47 +0100

    remove junk

--- TDD/AppDelegate.swift
+++ TDD/AppDelegate.swift
@@ -1,26 +1,11 @@
-//
-//  AppDelegate.swift
-//  TDD
-//
-//  Created by Maciek Rylko on 28.12.2016.
-//  Copyright © 2016 Maciek Rylko. All rights reserved.
-//
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
     }
-
-
 }
 
--- TDD/ViewController.swift
+++ TDD/ViewController.swift
@@ -1,27 +1,13 @@
-//
-//  ViewController.swift
-//  TDD
-//
-//  Created by Maciek Rylko on 28.12.2016.
-//  Copyright © 2016 Maciek Rylko. All rights reserved.
-//
-
 import Cocoa
 
 class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+        didSet {}
     }
-
-
 }
 
